95df580516a99bcbb8bcc15d5ae09108260dd1d0
liliang4869 <liliang4869@163.com>
2017-06-06 17:30:20 +0800

    增加测试按钮

--- index.android.js
+++ index.android.js
@@ -10,24 +10,33 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity, ToastAndroid
 } from 'react-native';
 import SideMenu from './src/index';
 export default class SideMenuTest extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <SideMenu height={500} width={300} Menu={this.Menu()} menuWidth={200}>
-        <View style={{flex:1,backgroundColor:'green'}}>
-          <TouchableOpacity style={{height:50,width:50,backgroundColor:'white'}} onPress={()=>{console.log('click')}}/>
+        <SideMenu height={500} width={300} Menu={this.Menu()} menuWidth={200}>
+          <View style={{ flex: 1, backgroundColor: 'white',justifyContent:'center',alignItems:'center',borderWidth:0.5,borderRadius:5 }}>
+            <TouchableOpacity style={{ height: 50, width: 200, backgroundColor: '#B0E2FF' ,justifyContent:'center',alignItems:'center'}} onPress={() => { ToastAndroid.show('主界面点击',400) }} >
+             <Text style={{ fontSize: 20 }}>点击测试</Text>
+              </TouchableOpacity>
           </View>
         </SideMenu>
-      
+
       </View>
     );
   }
-  Menu(){
-    return <View style={{height:500,width:200,backgroundColor:'red'}}/>
+  Menu() {
+    return <View style={{ height: 500, width: 200, backgroundColor: 'white',borderWidth:0.5,borderRadius:5 }}>
+      <TouchableOpacity style={{ height: 40, width: 200, backgroundColor: '#B0E2FF' }} onPress={() => { ToastAndroid.show('侧滑测试1', 400) }}><Text style={{ fontSize: 20 }}>测试1</Text></TouchableOpacity>
+      <View style={{ height: 1, width: 200, backgroundColor: '#eeeeee' }} />
+      <TouchableOpacity style={{ height: 40, width: 200, backgroundColor: '#B0E2FF' }} onPress={() => { ToastAndroid.show('侧滑测试2', 400) }}><Text style={{ fontSize: 20 }}>测试2</Text></TouchableOpacity>
+      <View style={{ height: 1, width: 200, backgroundColor: '#eeeeee' }} />
+      <TouchableOpacity style={{ height: 40, width: 200, backgroundColor: '#B0E2FF' }} onPress={() => { ToastAndroid.show('侧滑测试3', 400) }}><Text style={{ fontSize: 20 }}>测试3</Text></TouchableOpacity>
+      <View style={{ height: 1, width: 200, backgroundColor: '#eeeeee' }} />
+    </View>
   }
 }
 
