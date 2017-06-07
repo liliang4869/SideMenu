cd55a2adecc8ddec4d8f203b9c181bac0c169aef
liliang4869 <liliang4869@163.com>
2017-06-07 09:56:06 +0800

    侧滑菜单可选在在左侧或右侧

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class SideMenuTest extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <SideMenu height={500} width={300} Menu={this.Menu()} menuWidth={200}>
+        <SideMenu height={500} width={300} Menu={this.Menu()} menuWidth={200} menuLeft={false}>
           <View style={{ flex: 1, backgroundColor: '#b0b000',justifyContent:'center',alignItems:'center', }}>
             <TouchableOpacity style={{ height: 50, width: 200, backgroundColor: '#B0E2FF' ,justifyContent:'center',alignItems:'center'}} onPress={() => { ToastAndroid.show('主界面点击',400) }} >
              <Text style={{ fontSize: 20 }}>点击测试</Text>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component,PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,75 +7,114 @@ import {
   Dimensions,
   PanResponder
 } from 'react-native';
-var {height,width} =Dimensions.get('window');
-export default class SideMenu extends Component{
-constructor(props)
-{
+var { height, width } = Dimensions.get('window');
+export default class SideMenu extends Component {
+  constructor(props) {
     super(props);
-    this.state={offset:new Animated.Value(-1*this.props.menuWidth),open:false};
-    this.menuResponder=PanResponder.create({ 
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
+    this.state = { offset: this.props.menuLeft ? new Animated.Value(-1 * this.props.menuWidth) : new Animated.Value(0), open: false };
+    this.menuResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => false,
       onMoveShouldSetPanResponder: (evt, gestureState) => false,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,
 
       onPanResponderGrant: (evt, gestureState) => {
-        this.pre=this.state.offset._value;
+        this.pre = this.state.offset._value;
       },
       onPanResponderMove: (evt, gestureState) => {
-          let curLocation=gestureState.dx+this.pre;
-          if(curLocation<-1*this.props.menuWidth || curLocation>0)return;
-          else Animated.timing(this.state.offset,{toValue:curLocation,duration:0}).start();
+        let curLocation = gestureState.dx + this.pre;
+        console.log(gestureState.dx,this.pre)
+        if (this.props.menuLeft) {
+          if (curLocation < -1 * this.props.menuWidth || curLocation > 0) return;
+          else Animated.timing(this.state.offset, { toValue: curLocation, duration: 0 }).start();
+        }
+        else {
+          if (curLocation > 0 || curLocation < -1*this.props.menuWidth) return;
+          else Animated.timing(this.state.offset, { toValue: curLocation, duration: 0 }).start();
+        }
       },
       onPanResponderTerminationRequest: (evt, gestureState) => false,
       onPanResponderRelease: (evt, gestureState) => {
-        let curLocation=gestureState.dx+this.pre;
-        if(curLocation>-0.5*this.props.menuWidth){this.setState({open:true});Animated.spring(this.state.offset,{toValue:0}).start();}
-        else {this.setState({open:false});Animated.spring(this.state.offset,{toValue:-1*this.props.menuWidth}).start();}
-        
-      },});
-      this.PageResponder=PanResponder.create({ 
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
+        let curLocation = gestureState.dx + this.pre;
+        if (this.props.menuLeft) {
+          if (curLocation > -0.5 * this.props.menuWidth) { this.setState({ open: true}); Animated.spring(this.state.offset, { toValue: 0 }).start(); }
+          else { this.setState({ open: false }); Animated.spring(this.state.offset, { toValue: -1 * this.props.menuWidth }).start(); }
+        }
+        else {
+          if (curLocation < -0.5 * this.props.menuWidth) { this.setState({ open: true }); Animated.spring(this.state.offset, { toValue: -1*this.props.menuWidth }).start(); }
+          else { this.setState({ open: false }); Animated.spring(this.state.offset, { toValue: 0 }).start(); }
+
+        }
+      }
+    });
+    this.PageResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
       onStartShouldSetPanResponderCapture: (evt, gestureState) => this.state.open,
       onMoveShouldSetPanResponder: (evt, gestureState) => false,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,
 
       onPanResponderGrant: (evt, gestureState) => {
-        this.pre=this.state.offset._value;
+        this.pre = this.state.offset._value;
       },
       onPanResponderMove: (evt, gestureState) => {
-          let curLocation=gestureState.dx+this.pre;
-          if(curLocation<-1*this.props.menuWidth || curLocation>0)return;
-          else Animated.timing(this.state.offset,{toValue:curLocation,duration:0}).start();
+         let curLocation = gestureState.dx + this.pre;
+        console.log(gestureState.dx,this.pre)
+        if (this.props.menuLeft) {
+          if (curLocation < -1 * this.props.menuWidth || curLocation > 0) return;
+          else Animated.timing(this.state.offset, { toValue: curLocation, duration: 0 }).start();
+        }
+        else {
+          if (curLocation > 0 || curLocation < -1*this.props.menuWidth) return;
+          else Animated.timing(this.state.offset, { toValue: curLocation, duration: 0 }).start();
+        }
       },
       onPanResponderTerminationRequest: (evt, gestureState) => false,
       onPanResponderRelease: (evt, gestureState) => {
-        let curLocation=gestureState.dx+this.pre;
-        if(curLocation>-0.5*this.props.menuWidth){this.setState({open:true});Animated.spring(this.state.offset,{toValue:0}).start();}
-        else {this.setState({open:false});Animated.spring(this.state.offset,{toValue:-1*this.props.menuWidth}).start();}
-        
-      },});
-}
-render(){
+        let curLocation = gestureState.dx + this.pre;
+        if (this.props.menuLeft) {
+          if (curLocation > -0.5 * this.props.menuWidth) { this.setState({ open: true }); Animated.spring(this.state.offset, { toValue: 0 }).start(); }
+          else { this.setState({ open: false }); Animated.spring(this.state.offset, { toValue: -1 * this.props.menuWidth }).start(); }
+        }
+        else {
+          if (curLocation <- 0.5 * this.props.menuWidth) { this.setState({ open: true }); Animated.spring(this.state.offset, { toValue: -1*this.props.menuWidth }).start(); }
+          else { this.setState({ open: false }); Animated.spring(this.state.offset, { toValue: 0 }).start(); }
+
+        }
+
+      },
+    });
+  }
+  render() {
     console.log(this.state.open)
-return (<View
-        style={[{height:this.props.height,width:this.props.width,flexDirection: 'row',alignItems:'center',backgroundColor:'white'}]}>
-        <Animated.View style={{height:this.props.height,width:this.props.menuWidth,
-        transform:[{translateX:this.state.offset}]}}{...this.menuResponder.panHandlers}
-        >
+    return (<View
+      style={[{ height: this.props.height, width: this.props.width, flexDirection: 'row', alignItems: 'center', backgroundColor: 'white' }]}>
+      {this.props.menuLeft ? <Animated.View style={{
+        height: this.props.height, width: this.props.menuWidth,
+        transform: [{ translateX: this.state.offset }]
+      }}{...this.menuResponder.panHandlers}
+      >
         {this.props.Menu}
-        </Animated.View>
-        <Animated.View 
-        style={{height:this.props.height,width:this.props.width,backgroundColor:'white',
-        transform:[{translateX:this.state.offset}]}}
+      </Animated.View> : null}
+      <Animated.View
+        style={{
+          height: this.props.height, width: this.props.width, backgroundColor: 'white',
+          transform: [{ translateX: this.state.offset }]
+        }}
         {...this.PageResponder.panHandlers}>
-            {this.props.children}
-              {
-            this.state.open?
-        <View style={{height:this.props.height,width:this.props.height,backgroundColor:'#1010106f',position:'absolute',top:0,left:0}}/>:null}
-            </Animated.View>
-        </View>)
-}
+        {this.props.children}
+        {
+          this.state.open ?
+            <View style={{ height: this.props.height, width: this.props.height, backgroundColor: '#1010106f', position: 'absolute', top: 0, left: 0 }} /> : null}
+      </Animated.View>
+      {!this.props.menuLeft ? <Animated.View style={{
+        height: this.props.height, width: this.props.menuWidth,
+        transform: [{ translateX: this.state.offset }]
+      }}{...this.menuResponder.panHandlers}
+      >
+        {this.props.Menu}
+      </Animated.View> : null}
+    </View>)
+  }
 }
 // SideMenu.propTypes={
 //     Menu:PropTypes.element.isRequired,
@@ -83,9 +122,10 @@ return (<View
 //     width:PropTypes.number.isRequired,
 //     menuWidth:propTypes.number.isRequired
 // }
-SideMenu.defaultProps={
-    Menu:<View style={{height:height,width:width*.5}}/>,
-    height:height,
-    width:width,
-    menuWidth:width*0.5
+SideMenu.defaultProps = {
+  Menu: <View style={{ height: height, width: width * .5 }} />,
+  height: height,
+  width: width,
+  menuWidth: width * 0.5,
+  menuLeft: true
 }
